e6b7216fbe1c6c764f3fee68c41f920246312d7f
Justin Makeig <jmpublic+github@makeig.com>
2015-02-09 00:08:21 -0500

    Working demo of proxying console calls to a remote service

--- app/index.js
+++ app/index.js
@@ -1,14 +1,26 @@
 var LF = require('console-winston-syslog').LoggerFactory;
 var console = (new LF('Other')).logger;
+var browser = (new LF('Other')).logger;
+    browser.transports.posixSyslog.level = 'debug';
 
 var path = require('path');
-var express = require('express')
+var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express()
 app.use(express.static(path.join(__dirname, '../browser/public/')));
+
+app.post('/logger', bodyParser.json({type: 'application/json'}), function(req, res) {
+  res.sendStatus(202);
+  res.end();
+  var msg = req.body;
+  // FIXME: How do I differentiate these from the application-level logs?
+  browser[msg.level](msg.body);
+});
 
 var server = app.listen(3333, function () {
   var host = server.address().address
   var port = server.address().port
   console.notice('Example app listening at http://%s:%s', host, port)
-})
+});
+
---
+++ browser/public/RemoteConsole.js
@@ -0,0 +1,74 @@
+/**
+ * A wrapper around the console object that sends messages to a remote service.
+ * This is EXPERIMENTAL. This is only tested in Safari 8.0.2.
+ * There is a lot of hard-coding mostly to get around problems I had try to 
+ * apply functions for the console prototype.
+ */
+function RemoteConsole(console) {
+  var proxies = ['info', 'debug', 'error', 'warn', 'log'];
+  this.orig = window._console = console;
+  if(window.console === console) {
+    window.console = this;
+  }
+  this.log = this.info = function() {
+    if('undefined' !== this.orig) {
+      RemoteConsole.prototype.info.apply(this, arguments);
+    }
+    this.send.apply(this, ['info'].concat(Array.prototype.slice.call(arguments)));
+  };
+  this.debug = function() {
+    if('undefined' !== this.orig) {
+      RemoteConsole.prototype.debug.apply(this, arguments);
+    }
+    this.send.apply(this, ['debug'].concat(Array.prototype.slice.call(arguments)));
+  };
+  this.warn = function() {
+    if('undefined' !== this.orig) {
+      RemoteConsole.prototype.warn.apply(this, arguments);
+    }
+    this.send.apply(this, ['warn'].concat(Array.prototype.slice.call(arguments)));
+  };
+  this.error = function() {
+    if('undefined' !== this.orig) {
+      RemoteConsole.prototype.error.apply(this, arguments);
+    }
+    this.send.apply(this, ['error'].concat(Array.prototype.slice.call(arguments)));
+  };
+};
+RemoteConsole.prototype.assert = function() { if('function' === typeof console.assert) { return Function.prototype.bind.call(console.assert, console); } else { return function() {}}}();
+RemoteConsole.prototype.clear = function() { if('function' === typeof console.clear) { return Function.prototype.bind.call(console.clear, console); } else { return function() {}}}();
+RemoteConsole.prototype.count = function() { if('function' === typeof console.count) { return Function.prototype.bind.call(console.count, console); } else { return function() {}}}();
+RemoteConsole.prototype.debug = function() { if('function' === typeof console.debug) { return Function.prototype.bind.call(console.debug, console); } else { return function() {}}}();
+RemoteConsole.prototype.dir = function() { if('function' === typeof console.dir) { return Function.prototype.bind.call(console.dir, console); } else { return function() {}}}();
+RemoteConsole.prototype.dirxml = function() { if('function' === typeof console.dirxml) { return Function.prototype.bind.call(console.dirxml, console); } else { return function() {}}}();
+RemoteConsole.prototype.error = function() { if('function' === typeof console.error) { return Function.prototype.bind.call(console.error, console); } else { return function() {}}}();
+RemoteConsole.prototype.group = function() { if('function' === typeof console.group) { return Function.prototype.bind.call(console.group, console); } else { return function() {}}}();
+RemoteConsole.prototype.groupCollapsed = function() { if('function' === typeof console.groupCollapsed) { return Function.prototype.bind.call(console.groupCollapsed, console); } else { return function() {}}}();
+RemoteConsole.prototype.groupEnd = function() { if('function' === typeof console.groupEnd) { return Function.prototype.bind.call(console.groupEnd, console); } else { return function() {}}}();
+RemoteConsole.prototype.info = function() { if('function' === typeof console.info) { return Function.prototype.bind.call(console.info, console); } else { return function() {}}}();
+RemoteConsole.prototype.log = function() { if('function' === typeof console.log) { return Function.prototype.bind.call(console.log, console); } else { return function() {}}}();
+RemoteConsole.prototype.profile = function() { if('function' === typeof console.profile) { return Function.prototype.bind.call(console.profile, console); } else { return function() {}}}();
+RemoteConsole.prototype.profileEnd = function() { if('function' === typeof console.profileEnd) { return Function.prototype.bind.call(console.profileEnd, console); } else { return function() {}}}();
+RemoteConsole.prototype.table = function() { if('function' === typeof console.table) { return Function.prototype.bind.call(console.table, console); } else { return function() {}}}();
+RemoteConsole.prototype.time = function() { if('function' === typeof console.time) { return Function.prototype.bind.call(console.time, console); } else { return function() {}}}();
+RemoteConsole.prototype.timeEnd = function() { if('function' === typeof console.timeEnd) { return Function.prototype.bind.call(console.timeEnd, console); } else { return function() {}}}();
+RemoteConsole.prototype.timeStamp = function() { if('function' === typeof console.timeStamp) { return Function.prototype.bind.call(console.timeStamp, console); } else { return function() {}}}();
+RemoteConsole.prototype.trace = function() { if('function' === typeof console.trace) { return Function.prototype.bind.call(console.trace, console); } else { return function() {}}}();
+RemoteConsole.prototype.warn = function() { if('function' === typeof console.warn) { return Function.prototype.bind.call(console.warn, console); } else { return function() {}}}();
+
+// Set and forget. Intentionally no callback.
+RemoteConsole.prototype.send = function(level, body) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if(XMLHttpRequest.DONE === this.readyState) {}
+  }
+  xhr.open('POST', '/logger', true);
+  xhr.setRequestHeader('Content-type', 'application/json');
+  xhr.send(
+    JSON.stringify({
+      'level': level,
+      'body': Array.prototype.slice.call(arguments, 1).join(' ')
+    })
+  );  
+};
+
